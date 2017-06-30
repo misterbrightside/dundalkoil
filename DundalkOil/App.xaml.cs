8d7d056a7599d3be385c7fbea1020459bce0d33a
John Brennan <john.brennan55@mail.dcu.ie>
2017-06-30 14:38:03 -0700

    Add image, text box to window

--- DundalkOil/App.xaml.cs
+++ DundalkOil/App.xaml.cs
@@ -8,10 +8,12 @@ using System.Windows;
 
 namespace DundalkOil
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An exception just occurred: " + e.Exception.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
     }
 }
